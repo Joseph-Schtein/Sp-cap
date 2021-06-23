ccc4a3d04e66eb8985dafa36ccaf68cb64bfc443
Joseph-Schtein <57626922+Joseph-Schtein@users.noreply.github.com>
2021-06-23 15:56:55 +0300

    Add files via upload

--- api/views.py
+++ api/views.py
@@ -1,6 +1,10 @@
-from django.core import serializers
+import json
+import time
+
+import pytz
+from django.db import transaction
 from django.shortcuts import render
-from datetime import timedelta
+from datetime import timedelta, datetime, date
 from rest_framework import viewsets, status
 from django.utils import timezone
 from rest_framework.authentication import TokenAuthentication
@@ -8,12 +12,13 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from .models import Course, Course_group, Student, Ranking, Result, Office
-from django.core.serializers import serialize
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
 from .serializers import CourseSerializer, Course_groupSerializer, Course_groupMiniSerializer, StudentSerializer, \
     RankingSerializer, ResultSerializer, UserSerializer, OfficeSerializer, RankingMiniSerializer
 from api.SP_algorithm.main import main
+
+
 
 # take second element for sort
 def take_score(elem):
@@ -45,21 +50,25 @@ class Course_groupViewSet(viewsets.ModelViewSet):
         user = request.user
         student = Student.objects.get(user=user)
         student_office = Student.objects.get(user=user).office
-        ranking = Ranking.objects.filter(student=student).filter(course_group__is_elective=True) \
-            .filter(course_group__office=student_office).order_by('rank')
+        ranking = Ranking.objects.filter(student=student).filter(course__course_group__is_elective=True) \
+            .filter(course__course_group__office=student_office).order_by('rank')
         if len(ranking) == 0:  # the user not rank his courses
-            courses = Course_group.objects.filter(is_elective=True) \
-                .filter(office=student_office)
+            courses = Course.objects.filter(course_group__is_elective=True) \
+                .filter(course_group__office=student_office)
             default_ranking = []
             for course in courses:
-                rank = {"name": course.name, "score": 50}
+                rank = {"name": course.course_group.name, "lecturer": course.lecturer, "semester": course.Semester,
+                        "day": course.day, "time_start": course.time_start, "time_end": course.time_end, "score": 30,
+                        "id": course.course_id}
                 default_ranking.append(rank)
             serializer = RankingMiniSerializer(default_ranking, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:  # the user rank his courses, show his last rank
             user_ranking = []
             for rank in ranking:
-                rank = {"name": rank.course_group.name, "score": rank.rank}
+                rank = {"name": rank.course.course_group.name, "lecturer": rank.course.lecturer,
+                        "semester": rank.course.Semester, "day": rank.course.day, "time_start": rank.course.time_start,
+                        "time_end": rank.course.time_end, "score": rank.rank, "id": rank.course.course_id}
                 user_ranking.append(rank)
             user_ranking.sort(key=take_score, reverse=True)
             serializer = RankingMiniSerializer(user_ranking, many=True)
@@ -81,30 +90,110 @@ class CourseViewSet(viewsets.ModelViewSet):
         user = request.user
         office = Office.objects.get(user=user)
         # Read the JSON
-        titles = request.data['courses']
+        courses_string = request.data['courses']
+        try:
+            courses = json.loads(courses_string)
+        except json.decoder.JSONDecodeError as e:
+            return Response(str(e), status=status.HTTP_200_OK)
         # Create a Django model object for each object in the JSON
-        for course in titles['courses']:
-            course_group = Course_group.objects.get(name=course['name'])
-            Course.objects.create(course_id=course['id'], Semester=course['semester'], lecturer=course['lecturer'],
-                                  day=course['day'], capacity=course['capacity'], time_start=course['time_start'],
-                                  time_end=course['time_end'], course_group=course_group)
-        return Response('objects of courses created', status=status.HTTP_200_OK)
+        try:
+            with transaction.atomic():
+                for course in courses:
+                    print(course['start_time'])
+                    try:
+                        course_group = Course_group.objects.get(name=course['name'])
+                        Course.objects.create(course_id=course['id'], Semester=course['semester'],
+                                              lecturer=course['lecturer'],
+                                              day=course['day'], capacity=course['capacity'],
+                                              time_start=course['start_time'],
+                                              time_end=course['end_time'], course_group=course_group)
+                    except Course_group.DoesNotExist:
+                        course_group = Course_group.objects.create(name=course['name'],
+                                                                   is_elective=course['is_elective'], office=office)
+                        Course.objects.create(course_id=course['id'], Semester=course['semester'],
+                                              lecturer=course['lecturer'],
+                                              day=course['day'], capacity=course['capacity'],
+                                              time_start=course['start_time'],
+                                              time_end=course['end_time'], course_group=course_group)
+                return Response("הקורסים נוצרו", status=status.HTTP_200_OK)
+
+
+        except KeyError as e:
+            return Response("KeyError" + str(e), status=status.HTTP_200_OK)
 
     @action(detail=False, methods=['GET'])
     def get_semester_a(self, request):
         user = request.user
         student_office = Student.objects.get(user=user).office
-        courses = Course.objects.filter(Semester="א").filter(course_group__office=student_office)
+        courses = Course.objects.filter(Semester="א").filter(course_group__office=student_office).filter(
+            course_group__is_elective=True)
+        courses_semester_a = [[[], [], [], [], [], []], [[], [], [], [], [], []], [[], [], [], [], [], []],
+                              [[], [], [], [], [], []], [[], [], [], [], [], []], [[], [], [], [], [], []],
+                              [[], [], [], [], [], []], [[], [], [], [], [], []], [[], [], [], [], [], []],
+                              [[], [], [], [], [], []], [[], [], [], [], [], []], [[], [], [], [], [], []],
+                              [[], [], [], [], [], []], [[], [], [], [], [], []], [[], [], [], [], [], []]]
         serializer = CourseSerializer(courses, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        for course in serializer.data:
+            time_start = datetime.strptime(course['time_start'], '%H:%M:%S').time()
+            time_end = datetime.strptime(course['time_end'], '%H:%M:%S').time()
+            duration = str(datetime.combine(date.today(), time_end) - datetime.combine(date.today(), time_start))
+            course['duration'] = duration[0]
+            for i in range(14):
+                start_hour = i + 8
+                start_table = '0' + str(start_hour) + ':00'
+                if start_hour > 9:
+                    start_table = str(start_hour) + ':00'
+                if time_start == datetime.strptime(start_table, '%H:%M').time():
+                    if course['day'] == 'א':
+                        courses_semester_a[i][5].append(course)
+                    if course['day'] == 'ב':
+                        courses_semester_a[i][4].append(course)
+                    if course['day'] == 'ג':
+                        courses_semester_a[i][3].append(course)
+                    if course['day'] == 'ד':
+                        courses_semester_a[i][2].append(course)
+                    if course['day'] == 'ה':
+                        courses_semester_a[i][1].append(course)
+                    if course['day'] == 'ו':
+                        courses_semester_a[i][0].append(course)
+        return Response(courses_semester_a, status=status.HTTP_200_OK)
 
     @action(detail=False, methods=['GET'])
     def get_semester_b(self, request):
         user = request.user
         student_office = Student.objects.get(user=user).office
-        courses = Course.objects.filter(Semester="ב").filter(course_group__office=student_office)
+        courses = Course.objects.filter(Semester="ב").filter(course_group__office=student_office).filter(
+            course_group__is_elective=True)
+        courses_semester_a = [[[], [], [], [], [], []], [[], [], [], [], [], []], [[], [], [], [], [], []],
+                              [[], [], [], [], [], []], [[], [], [], [], [], []], [[], [], [], [], [], []],
+                              [[], [], [], [], [], []], [[], [], [], [], [], []], [[], [], [], [], [], []],
+                              [[], [], [], [], [], []], [[], [], [], [], [], []], [[], [], [], [], [], []],
+                              [[], [], [], [], [], []], [[], [], [], [], [], []], [[], [], [], [], [], []]]
         serializer = CourseSerializer(courses, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        for course in serializer.data:
+            time_start = datetime.strptime(course['time_start'], '%H:%M:%S').time()
+            time_end = datetime.strptime(course['time_end'], '%H:%M:%S').time()
+            duration = str(datetime.combine(date.today(), time_end) - datetime.combine(date.today(), time_start))
+            course['duration'] = duration[0]
+            for i in range(14):
+                start_hour = i + 8
+                start_table = '0' + str(start_hour) + ':00'
+                if start_hour > 9:
+                    start_table = str(start_hour) + ':00'
+                if time_start == datetime.strptime(start_table, '%H:%M').time():
+                    if course['day'] == 'א':
+                        courses_semester_a[i][5].append(course)
+                    if course['day'] == 'ב':
+                        courses_semester_a[i][4].append(course)
+                    if course['day'] == 'ג':
+                        courses_semester_a[i][3].append(course)
+                    if course['day'] == 'ד':
+                        courses_semester_a[i][2].append(course)
+                    if course['day'] == 'ה':
+                        courses_semester_a[i][1].append(course)
+                    if course['day'] == 'ו':
+                        courses_semester_a[i][0].append(course)
+        return Response(courses_semester_a, status=status.HTTP_200_OK)
 
     @action(detail=False, methods=['GET'])
     def get_semester_s(self, request):
@@ -139,20 +228,26 @@ class StudentViewSet(viewsets.ModelViewSet):
         user = request.user
         office = Office.objects.get(user=user)
         # Read the JSON
-        titles = request.data['students']
+        students_string = request.data['students']
+        try:
+            students = json.loads(students_string)
+        except json.decoder.JSONDecodeError:
+            return Response('The file is invalid', status=status.HTTP_200_OK)
         # Create a Django model object for each object in the JSON
-        for student in titles['students']:
-            student_user = User.objects.create_user(username=student['name'], password=student['password'],
-                                                    email=student['email'])
-            Token.objects.create(user=student_user)
-            student_obj = Student.objects.create(user=student_user, student_id=student['id'],
-                                                 amount_elective=student['amount_elective'], office=office)
-            student_obj.save()
-            for course in student['courses']:
-                cur = Course.objects.get(course_id=course)
-                student_obj.courses.add(cur)
+        try:
+            with transaction.atomic():
+                for student in students:
 
-        return Response('objects of student  created', status=status.HTTP_200_OK)
+                    student_user = User.objects.create_user(student['name'], student['email'], student['password'])
+                    Token.objects.create(user=student_user)
+                    student_obj = Student.objects.create(user=student_user, student_id=student['id'],
+                                                         amount_elective=student['amount_elective'], office=office)
+                    for course in student['courses']:
+                        cur = Course.objects.get(course_id=course)
+                        student_obj.courses.add(cur)
+        except KeyError as e:
+            return Response("KeyError" + str(e), status=status.HTTP_200_OK)
+        return Response('Students created', status=status.HTTP_200_OK)
 
 
 class OfficeViewSet(viewsets.ModelViewSet):
@@ -169,11 +264,31 @@ class OfficeViewSet(viewsets.ModelViewSet):
         student_serializer = StudentSerializer(student_set, many=True)
         course_set = Course_group.objects.filter(office=office)
         course_serializer = Course_groupSerializer(course_set, many=True)
+
+        # We create a default ranking for students that didn't ranked
+
+        course_set_tmp = Course.objects.filter(course_group__office=office, course_group__is_elective=True)
+        course_serializer_tmp = CourseSerializer(course_set_tmp, many=True)
+        default_rank = int(1000 / len(course_serializer_tmp.data))
+
+        for index in student_serializer.data:
+             s = Student.objects.get(student_id=index['student_id'])
+             count_ranking = Ranking.objects.filter(student=s)
+             if len(count_ranking) == 0:
+                 for co in course_serializer_tmp.data:
+                     cg = Course.objects.get(course_id=co['course_id'])
+                     Ranking.objects.create(course=cg, student=s, rank=default_rank)
+
         ranking_set = Ranking.objects.filter(student__office=office)
         ranking_serializer = RankingSerializer(ranking_set, many=True)
-        main(student_serializer.data, course_serializer.data, ranking_serializer.data)
+        students, courses = main(student_serializer.data, course_serializer.data, ranking_serializer.data)
+        for student in students:
+            for course in courses:
+                if student.if_student_enroll(course.get_name()):
+                    s = Student.objects.get(student_id=student.get_id())
+                    c = Course.objects.get(course_id=course.get_id())
+                    Result.objects.create(course=c, student=s, selected=True)
         return Response("OK", status=status.HTTP_200_OK)
-
 
     @action(detail=False, methods=['GET'])
     def get_time(self, request):
@@ -201,6 +316,20 @@ class OfficeViewSet(viewsets.ModelViewSet):
         response = {'message': time, 'value': 1}
         return Response(response, status=status.HTTP_200_OK)
 
+    @action(detail=False, methods=['POST'])
+    def set_date(self, request):
+        user = request.user
+        start_date = request.data['StartDate'].split('-')
+        end_date = request.data['EndDate'].split('-')
+        start_time = request.data['StartTime'].split(':')
+        end_time = request.data['EndTime'].split(':')
+        start_datetime = datetime(int(start_date[0]), int(start_date[1]), int(start_date[2]), int(start_time[0]),
+                                  int(start_time[1]), 0, tzinfo=pytz.UTC) - timedelta(hours=3)
+        end_datetime = datetime(int(end_date[0]), int(end_date[1]), int(end_date[2]), int(end_time[0]),
+                                int(end_time[1]), 0, tzinfo=pytz.UTC) - timedelta(hours=3)
+        Office.objects.filter(user=user).update(start_time=start_datetime, end_time=end_datetime)
+        return Response("התאריכים התעדכנו", status=status.HTTP_200_OK)
+
 
 class RankingViewSet(viewsets.ModelViewSet):
     queryset = Ranking.objects.all()
@@ -220,16 +349,15 @@ class RankingViewSet(viewsets.ModelViewSet):
             return Response(response, status=status.HTTP_400_BAD_REQUEST)
         if len(is_rated) == 0:  # the user not rank his courses (create)
             for movie_rank in ranking:
-                course = Course_group.objects.get(name=movie_rank['name'])
-                Ranking.objects.create(course_group=course, student=student, rank=movie_rank['score'])
+                course = Course.objects.get(course_id=movie_rank['id'])
+                Ranking.objects.create(course=course, student=student, rank=movie_rank['score'])
             return Response('Ranking created', status=status.HTTP_200_OK)
         else:  # the user rank his courses (update)
             for movie_rank in ranking:
-                last_rank = Ranking.objects.get(course_group__name=movie_rank['name'], student=student)
-                if last_rank.rank != movie_rank['score']:
-                    last_rank.rank = movie_rank['score']
-                    last_rank.save()
+                course = Course.objects.get(course_id=movie_rank['id'])
+                Ranking.objects.filter(course=course, student=student).update(rank=movie_rank['score'])
             return Response('Ranking updated', status=status.HTTP_200_OK)
+        return Response('ok', status=status.HTTP_200_OK)
 
     def update(self, request, *args, **kwargs):
         response = {'message': 'לא ניתן לעדכן דירוג באופן זה'}
@@ -245,3 +373,15 @@ class ResultViewSet(viewsets.ModelViewSet):
     serializer_class = ResultSerializer
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
+
+    @action(detail=False, methods=['GET'])
+    def get_results(self, request):
+        user = request.user
+        student = Student.objects.get(user=user)
+        results = Result.objects.filter(student=student).filter(selected=True)
+        serializer = ResultSerializer(results, many=True)
+        courses = []
+        for result in serializer.data:
+            courses.append(Course.objects.get(id=result['course']))
+        serializer = CourseSerializer(courses, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
